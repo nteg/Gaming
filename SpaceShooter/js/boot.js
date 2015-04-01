19f16cb6e1211d4cda7049493d7d7993ca04bc9b
ravisharma <ravi.sharma@nagarro.com>
2015-04-02 01:26:00 +0530

    arrow keys movement added

---
+++ SpaceShooter/js/boot.js
@@ -0,0 +1,26 @@
+ var SpaceShooter = {};
+
+SpaceShooter.Boot = function(game) {};
+
+SpaceShooter.Boot.prototype = {
+    preload: function() {
+        this.load.image('preloaderBar', 'images/loader_bar.png');
+    },
+    
+    create: function() {
+        this.input.maxPointers = 1;
+        this.stage.disableVisibilityChange = false;
+        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        this.scale.minWidth = 270;
+        this.scale.minHeight = 480;
+        this.scale.pageAlignHorizontally = true;
+        this.scale.pageAlignVertically = true;
+        this.stage.forcePortrait = true;
+        this.scale.setScreenSize(true);
+
+        this.input.addPointer();
+        this.stage.backgroundColor = '#171642';
+        
+        this.state.start('Preloader');
+    }
+}
--- SpaceShooter/js/game.js
+++ SpaceShooter/js/game.js
@@ -1,6 +1,7 @@
 SpaceShooter.Game = function(game) {
     this.far;
     this.ship;
+    this.cursors;
 }
 
 SpaceShooter.Game.prototype = {
@@ -14,7 +15,9 @@ SpaceShooter.Game.prototype = {
         ship = this.add.sprite(10,200,'player'); 
     
         this.physics.enable(ship,Phaser.Physics.ARCADE);
-        this.camera.follow(ship);
+        arrowKeys = this.input.keyboard.createCursorKeys();
+
+      //  this.camera.follow(ship);
         
         // ship.body.velocity.x=20;
     },
@@ -23,15 +26,29 @@ SpaceShooter.Game.prototype = {
 
         background.tilePosition.x -= 1;  
 
-
-        //  If the ship is > 8px away from the pointer then let's move to it.
-        if (this.physics.arcade.distanceToPointer(ship, this.input.activePointer) > 8) {
+        // mouse cursor movement 
+        //  If the ship is > 8px away from the pointer then let's move to it.        
+        /*if (this.physics.arcade.distanceToPointer(ship, this.input.activePointer) > 8) {
             //  Make the object seek to the active pointer (mouse or touch).
             this.physics.arcade.moveToPointer(ship, 300);
         } else {
             //  Otherwise turn off velocity because we're close enough to the pointer
             ship.body.velocity.set(0);
-        }
+        }*/
+        
+        // keyboard arrow keys movement 
+        // Reset the player, then check for movement keys
+        ship.body.velocity.setTo(0, 0);
 
+        if (arrowKeys.left.isDown) {
+            ship.body.velocity.x = -200;
+        } else if (arrowKeys.right.isDown) {
+            ship.body.velocity.x = 200;
+        } else if (arrowKeys.down.isDown) {
+            ship.body.velocity.y = 200;
+        } else if (arrowKeys.up.isDown) {
+            ship.body.velocity.y = -200;
+        }
     }
+
 };
