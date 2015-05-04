51ef0d63bf330f88fa6f2720c90850616653761f
Akshay Chauhan <akschhn@gmail.com>
2015-05-04 23:02:42 +0530

    obstacles and point blocks added

--- colorCrazy/main.js
+++ colorCrazy/main.js
@@ -1,4 +1,4 @@
-var balls = new Array();
+//var balls = new Array();
 
 var SimpleGame = (function () {
     function SimpleGame() {
@@ -11,8 +11,12 @@ var SimpleGame = (function () {
         //this.game.load.atlasXML('cars', 'assets/sheet_allCars.png', 'assets/sheet_allCars.xml');
         this.game.load.atlasXML('cars', 'assets/monsterTruck.png', 'assets/monsterTruck.xml');
         this.game.load.spritesheet('road', 'assets/road.png');
-		this.game.load.spritesheet('balls','assets/balls_spritesheet.png',259,259,4);
-		
+        
+        //this.game.load.spritesheet('balls','assets/balls_spritesheet.png',259,259,4);
+        
+        this.game.load.image('yellowBlock','assets/yellowBlock.png');
+        this.game.load.image('greenBlock','assets/greenBlock.png');
+
         this.game.load.audio('jumpSound', ['assets/GU-StealDaisy.mp3', 'assets/GU-StealDaisy.ogg']);
     };
 
@@ -24,8 +28,8 @@ var SimpleGame = (function () {
 
         this.background = this.game.add.sprite(0,0,'sky');
         //this.background.tint = 16711680;
-		
-		this.score = 0;  
+        
+        this.score = 0;  
         this.labelScore = this.game.add.text(10, 10, "0", { font: "30px Arial", fill: "#ffffff" });
 
         /*
@@ -92,12 +96,12 @@ var SimpleGame = (function () {
         this.road.autoScroll(-250,0);
 
         this.truck = this.game.add.sprite(0, 0, 'cars');
-        this.truck.scale.x = .3;
-        this.truck.scale.y = .3;
+        this.truck.scale.x = .2;
+        this.truck.scale.y = .2;
         //this.truck.animations.add('rainbow', [105, 204, 300, 392, 489], 1, true);
-        this.truck.animations.add('rainbow', [0, 1, 2, 3, 4, 5, 6, 7, 8], 0.1, true);
+        this.truck.animations.add('rainbow', [0, 1, 2, 3, 4, 5, 6, 7, 8], 0.5, true);
 
-        for(index = 0; index < 4; index++){
+        /*for(index = 0; index < 4; index++){
             balls[index] = new Array();
             balls[index] = this.game.add.group();
             balls[index].enableBody = true;
@@ -107,7 +111,7 @@ var SimpleGame = (function () {
             balls[index].scale.y = 0.7;
             balls[index].setAll('outOfBoundsKill', true);
             balls[index].setAll('checkWorldBounds', true);
-        }
+        }*/
 
         this.game.physics.arcade.enable([this.road, this.truck]);
         
@@ -119,17 +123,26 @@ var SimpleGame = (function () {
         this.truck.body.gravity.y = 800;
         this.truck.body.bounce.y = 0.4;
         this.truck.body.velocity.x = 50;
-        this.truck.body.maxVelocity.x = 100;
-		
+        this.truck.body.maxVelocity.x = 200;
+        
         // this.firstBall.body.velocity.x = -250;
 
         cursors = this.game.input.keyboard.createCursorKeys();
+
+        addBlocksToScene(this);
+        this.timer = this.game.time.events.loop(2000,addBlocks,this);
     };
 
     SimpleGame.prototype.update = function () {
         
         this.game.physics.arcade.collide(this.road, this.truck);
-        this.game.physics.arcade.collide(balls, this.truck);
+        //this.game.physics.arcade.collide(balls, this.truck);       
+        this.game.physics.arcade.collide(this.road,this.pointBlocks);
+        this.game.physics.arcade.collide(this.road,this.obstacleBlocks);
+        
+        this.game.physics.arcade.overlap(this.truck,this.pointBlocks,collectPoints,null,this);
+        this.game.physics.arcade.overlap(this.truck,this.obstacleBlocks, truckObstacleCollision,null,this);
+        
 
         this.truck.animations.play('rainbow');
 
@@ -153,22 +166,22 @@ var SimpleGame = (function () {
                 this.truck.body.velocity.x = 0;
         }
         else if (cursors.right.isDown) {
-            this.truck.body.velocity.x = 350;
+            this.truck.body.velocity.x = 200;
         }
         else if (this.game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
-            if(this.truck.body.y == 410) {
+            if(this.truck.body.y > 430) {
                 
                 this.truck.body.velocity.y = -500;
                 this.jumpSound.play();
             }
         }
 
-		if(this.truck.inWorld==false)
+        if(this.truck.inWorld==false)
         {
-            restartGame(this.truck,this.score,this.labelScore);
+            restartGame(this);
         }
 
-        for (index = 0; index < 4; index++){
+        /*for (index = 0; index < 4; index++){
             
             //  Grab the first bullet we can from the pool
             ball = balls[index].getFirstExists(false);
@@ -179,25 +192,94 @@ var SimpleGame = (function () {
                 ball.body.velocity.x = -250;
                 ball.body.immovable = true;
             }
-        }
+        }*/
     };
 
     return SimpleGame;
 })();
 
-var restartGame=function(truck,score,labelScore){
-//Show score and restart
-    truck.x=0; 
-    truck.y=0;
-    //var game=new SimpleGame();
-    score=0;
-    labelScore.text=score;
+var addBlocksToScene=function(scene){
 
+        //Create Obstacle Block Group
+        scene.obstacleBlocks=scene.game.add.group();
+        scene.obstacleBlocks.enableBody=true;
+        scene.obstacleBlocks.createMultiple(2000,'yellowBlock');
+        
+
+        //Create Point Block Group
+        scene.pointBlocks=scene.game.add.group(); //create a group
+        scene.pointBlocks.enableBody=true; //Add physics to the group
+        scene.pointBlocks.createMultiple(2000,'greenBlock');
+
+};
+
+var addBlocks= function() {  
+     
+     //Adding green point block   
+     for (var i = 0; i < 1; i++)
+     {
+        var randomNumber = Math.floor(Math.random() * 120) ; 
+
+        pointBlock=this.pointBlocks.getFirstDead();
+        pointBlock.body.gravity.y=120;
+        pointBlock.body.bounce.y=0.6 + Math.random()*0.1;
+        pointBlock.scale.setTo(0.3,0.3);        
+        pointBlock.reset(1300+randomNumber,randomNumber+100);
+        pointBlock.body.velocity.x=-300;            
+        pointBlock.body.angularVelocity=-50;
+        pointBlock.checkWorldBounds=true;
+        pointBlock.outOfBoundsKill=true;
+
+
+      }       
+
+    //Adding yellow  obstacle block
+    for(var j=0;j<1;j++)
+    {
+        var randomNumber=Math.floor(Math.random() * 50);
+        
+        obstacleBlock=this.obstacleBlocks.getFirstDead();
+        obstacleBlock.body.gravity.y=100;
+        obstacleBlock.body.bounce.y=0.5 + Math.random()*0.1;
+        obstacleBlock.scale.setTo(0.3,0.3); 
+        //obstacleBlock.reset(1300+randomNumber,randomNumber+105);
+        obstacleBlock.reset(1350+randomNumber,randomNumber+100);
+        obstacleBlock.body.velocity.x=-(300+(Math.random()*50));  
+        obstacleBlock.body.angularVelocity=-50;             
+        obstacleBlock.checkWorldBounds=true;
+        obstacleBlock.outOfBoundsKill=true;
+    }
+
+};
+
+var restartGame=function(game){  
+    game.truck.x=0; 
+    game.truck.y=0;
+    
+    game.score=0;
+    game.labelScore.text=game.score;
+
+    game.obstacleBlocks.destroy();
+    game.pointBlocks.destroy();
+
+    addBlocksToScene(game);
 };
 
 var resetBody = function (obj, parentSpeed) {
     obj.x = parentSpeed * 10;
-}
+};
+
+var collectPoints=function(truck,pointBlock){        
+        pointBlock.kill();        
+        this.score += 1;  
+        this.labelScore.text = this.score;
+};
+
+var truckObstacleCollision=function(truck,obstacleBlock){
+    obstacleBlock.kill();
+    restartGame(this);
+};
+
 
 window.onload = function () {
     var game = new SimpleGame();
