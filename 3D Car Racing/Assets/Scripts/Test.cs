e4fb04279e9cb187ba95d1e7b96362f8969e43d0
ravisharma <ravi.sharma@nagarro.com>
2016-05-06 23:18:39 +0530

    Committing basic 2 car system with one opponent AI car running on the waypoints

---
+++ 3D Car Racing/Assets/Scripts/Test.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Hello world");
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ 3D Car Racing/Assets/Scripts/Waypoints.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Waypoints : MonoBehaviour
+{
+    public static Waypoints start;
+    public Waypoints next;
+    public bool isStart = false;
+
+    void Awake()
+    {
+        if (!next)
+        {
+            Debug.LogWarning("This waypoint is not connected!");
+        }
+
+        if (isStart)
+        {
+            start = this;
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(transform.position, new Vector3(0.5f, 0.5f, 0.5f));
+
+        if (next)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(transform.position, next.transform.position);
+        }
+    }
+
+}
